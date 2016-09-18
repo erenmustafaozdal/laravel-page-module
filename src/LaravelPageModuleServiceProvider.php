f32f27edd890d6774303c5ab88cfae6e9a0b0f7f
erenMustafaOzdal <eren.060737@gmail.com>
2016-09-18 18:33:42 +0000

    page module merge default config

--- src/LaravelPageModuleServiceProvider.php
+++ src/LaravelPageModuleServiceProvider.php
@@ -39,10 +39,59 @@ class LaravelPageModuleServiceProvider extends ServiceProvider
         $this->mergeConfigFrom(
             __DIR__.'/../config/laravel-page-module.php', 'laravel-page-module'
         );
+        // merge default configs with publish configs
+        $this->mergeDefaultConfig();
 
         $router = $this->app['router'];
         // model binding
         $router->model(config('laravel-page-module.url.page'),  'App\Page');
         $router->model(config('laravel-page-module.url.page_category'),  'App\PageCategory');
+    }
+
+    /**
+     * merge default configs with publish configs
+     */
+    protected function mergeDefaultConfig()
+    {
+        $config = $this->app['config']->get('laravel-page-module', []);
+        $default = require __DIR__.'/../config/default.php';
+
+        // admin page category routes
+        $route = $config['routes']['admin']['page_category'];
+        $default['routes']['admin']['page_category'] = $route;
+        // admin page routes
+        $route = $config['routes']['admin']['page'];
+        $default['routes']['admin']['page'] = $route;
+        $default['routes']['admin']['page_publish'] = $route;
+        $default['routes']['admin']['page_notPublish'] = $route;
+        // admin sub page categories pages
+        $route = $config['routes']['admin']['sub_category_pages'];
+        $default['routes']['admin']['category_pages'] = $route;
+        $default['routes']['admin']['category_pages_publish'] = $route;
+        $default['routes']['admin']['category_pages_notPublish'] = $route;
+
+        // api page category routes
+        $route = $config['routes']['api']['page_category'];
+        $default['routes']['api']['page_category'] = $route;
+        $default['routes']['api']['page_category_models'] = $route;
+        $default['routes']['api']['page_category_group'] = $route;
+        $default['routes']['api']['page_category_detail'] = $route;
+        $default['routes']['api']['page_category_fastEdit'] = $route;
+        // api page routes
+        $route = $config['routes']['api']['page'];
+        $default['routes']['api']['page'] = $route;
+        $default['routes']['api']['page_group'] = $route;
+        $default['routes']['api']['page_detail'] = $route;
+        $default['routes']['api']['page_fastEdit'] = $route;
+        $default['routes']['api']['page_publish'] = $route;
+        $default['routes']['api']['page_notPublish'] = $route;
+        $default['routes']['api']['page_contentUpdate'] = $route;
+        // api sub page categories pages
+        $route = $config['routes']['api']['sub_category_pages'];
+        $default['routes']['api']['category_pages_index'] = $route;
+
+        $config['routes'] = $default['routes'];
+
+        $this->app['config']->set('laravel-page-module', $config);
     }
 }
